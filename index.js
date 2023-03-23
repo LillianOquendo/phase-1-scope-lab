b271e6dbd6892775324efb2020c7dd7c30e2dc06
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-03-23 10:44:52 -0400

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName (){
+ customerName = customerName.toUpperCase();
+return customerName
+}
+
+function setBestCustomer(){
+ var bestCustomer = 'not bob'   
+ return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = 'Lillian'
+
+function changeLeastFavoriteCustomer (){
+    leastFavoriteCustomer = 'Bob'
+    return leastFavoriteCustomer
+}
